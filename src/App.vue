5136a57501708d32e3c9e8dcc6af7c24c4e61ec9
darklordstudios <84936475+darklordstudios@users.noreply.github.com>
2025-02-23 08:19:13 -0500

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,20 @@
 <!-- eslint-disable @typescript-eslint/no-unused-vars -->
 <script setup lang="ts">
 import { ref, onMounted } from 'vue'
+import { useSettingsStore } from './stores/settings'
+
+const appSettings = useSettingsStore()
 
 onMounted(() => {
   // setup global variables in settings store
+  appSettings.siteadmin = globalThis.spcontext.pageContext.legacyPageContext.isSiteAdmin
+  appSettings.fullcontrol = globalThis.hasManageWebPermissions
+  appSettings.displayname = globalThis.spcontext.pageContext.user.displayName
+  appSettings.loginname = globalThis.spcontext.pageContext.user.loginName
+  appSettings.instanceid = globalThis.spcontext.instanceId
+  appSettings.email = globalThis.spcontext.pageContext.user.email
+  appSettings.siteurl = globalThis.spcontext.pageContext.site.absoluteUrl
+  appSettings.weburl = globalThis.spcontext.pageContext.web.absoluteUrl
 })
 </script>
 
