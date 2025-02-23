4d559111f0df8d0847f95f29ef5c4e9116672a2c
darklordstudios <84936475+darklordstudios@users.noreply.github.com>
2025-02-23 08:23:49 -0500

    Update settings.ts

--- src/stores/settings.ts
+++ src/stores/settings.ts
@@ -1,2 +1,24 @@
 import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
+
+export const useSettingsStore = defineStore('settings', () => {
+  const instanceid = ref('')
+  const siteurl = ref('')
+  const weburl = ref('')
+  const fullcontrol = ref(false)
+  const displayname = ref('')
+  const loginname = ref('')
+  const email = ref('')
+  const siteadmin = ref(false)
+
+  return {
+    instanceid,
+    siteurl,
+    weburl,
+    fullcontrol,
+    displayname,
+    loginname,
+    email,
+    siteadmin
+  }
+})
